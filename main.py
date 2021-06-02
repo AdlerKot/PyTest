0eb2f2ac0f2e9e74167503d197366e6bda8437eb
Adler <pashko.adler@gmail.com>
2021-06-03 00:53:59 +0300

    Add 00-01 and README.md

--- main.py
+++ main.py
@@ -1,3 +1,62 @@
-print("test")
-print('123')
-print(3)
+# #00-00
+# print("Task 00-00")
+# print("Hello world! It is I, Arthur, son of Uther Pendragon, from the castle of Camelot.")
+# print()
+#
+# #00-01
+# print("Task 00-01")
+# print(50)
+# print(-42.00001)
+# print("Monty Python")
+# print(True)
+# print()
+#
+# #00-02
+# print("Task 00-02")
+# my_int=1
+# my_float=2.123
+# my_str="Tis but scratch"
+# my_bool=False
+# print(type(my_int))
+# print(type(my_float))
+# print(type(my_str))
+# print(type(my_bool))
+# print()
+#
+# #00-03
+# print("Task 00-03")
+# first_name = "Monty"
+# last_name = "Python"
+# full_name = first_name + " " + last_name
+# print(full_name)
+# print()
+#
+# #00-04
+# print("Task 00-04")
+# print("What is your name?")
+# name = input()
+# print("What is your quest?")
+# quest = input()
+# print("What is your favorite color?")
+# color = input()
+# print(f"BRIDGEKEEPER: Stop.\nWho would cross the Bridge of Death must answer me these questions three.\n"
+#       f"BRIDGEKEEPER: What is your name?\n{name}\nBRIDGEKEEPER: What is your quest?\n{quest}\nBRIDGEKEEPER: What is your favorite color?\n{color}\n"
+#       f"----------\nTraveler info:\nYour name is Sir Lancelot of Camelot\nYour quest is to seek the Holy Grail\n"
+#       "Your favorite color is blue\n----------\nBRIDGEKEEPER: Right. Off you go.")
+#
+# #00-05
+# a = input('Enter the first number: ')
+# b = input('Enter the second number: ')
+# a = int(a)
+# b = int(b)
+# print(f"{a} + {b} = {a+b}")
+# print(f"{a} - {b} = {a-b}")
+# print(f"{a} * {b} = {a*b}")
+# print(f"{a} / {b} = {a/b}")
+# print(f"{a} % {b} = {a%b}")
+# print(f"{a} ** {b} = {a**b}")
+# print(f"{a} // {b} = {a//b}")
+#
+# 00-06
+print("Task 00-06")
+print(123)
